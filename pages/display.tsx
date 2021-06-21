4d161566431f76c446e832cf5e75a7aac1ebb118
Rodger <rodger0531@gmail.com>
2021-06-21 20:58:50 +0800

    remove unused conditional

--- pages/display.tsx
+++ pages/display.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Display({ value }: { value: string }) {
   return (
     <div className="font-digital text-8xl w-full text-right my-8 px-6">
-      {value || 0}
+      {value}
     </div>
   );
 }
