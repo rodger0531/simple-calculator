b82f6815e005981d8bf9673f3681bc14b3a1273b
Rodger <rodger0531@gmail.com>
2021-06-21 21:23:13 +0800

    add expression to display

--- pages/display.tsx
+++ pages/display.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 
-export default function Display({ value }: { value: string }) {
+export default function Display({
+  value,
+  expression,
+}: {
+  value: string;
+  expression: string;
+}) {
   return (
-    <div className="font-digital text-8xl w-full text-right my-8 px-6">
-      {value}
+    <div className="font-digital w-full text-right my-8 px-6 text-white">
+      <div className="text-xl h-12">{expression}</div>
+      <div className="text-8xl">{value}</div>
     </div>
   );
 }
